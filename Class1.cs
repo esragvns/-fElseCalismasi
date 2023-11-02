4ce5061ccb82fdb4dadf98cfcd443d9ddb868921
Semra Güneş <60344956+Semra4141@users.noreply.github.com>
2023-11-02 22:20:26 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		int i = 1;
+		int j = 2;
+
+        if (i > j)
+        {
+			Console.Write("i büyükkk");
+		}
+
+        else (j > i){
+			Console.Write("j büyük");
+
+		}
+	}
+	public Class1()
+	{
+
+		int i = 1;
+		int j = 2;
+
+		if (i > j)
+		{
+			Console.Write("i büyükkk");
+		}
+
+		else (j > i){
+			Console.Write("j büyük");
+
+		}
+	}
+	public Class1()
+	{
+
+		int i = 1;
+		int j = 2;
+
+		if (i > j)
+		{
+			Console.Write("i büyükkk");
+		}
+
+		else (j > i){
+			Console.Write("j büyük");
+
+		}
+	}
+	public Class1()
+	{
+
+		int i = 1;
+		int j = 2;
+
+		if (i > j)
+		{
+			Console.Write("i büyükkk");
+		}
+
+		else (j > i){
+			Console.Write("j büyük");
+
+		}
+	}
+}
